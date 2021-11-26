32fe8e10fe367197116052dd2850ca34c9920791
bilalahmed-dev0 <85331351+bilalahmed-dev0@users.noreply.github.com>
2021-11-26 18:11:17 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+/*const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+*/
+let toggleButton = document.getElementsByClassName('burger')[0]
+let navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click',() =>{
+  navbarLinks.classList.toggle('active')
+})
